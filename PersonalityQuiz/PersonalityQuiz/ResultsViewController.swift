07a3110d39b04cf6adb62adc8baaeb4760c00379
miloucarmen <36204209+miloucarmen@users.noreply.github.com>
2018-05-14 18:25:30 +0200

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Gebruiker on 13-05-18.
+//  Copyright © 2018 Gebruiker. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    var responses: [Answer]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType:Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+
+}
